aea2c4b6740830f931040f6787b822d9fa91b046
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2023-11-22 22:40:13 +0200

    test(Makefile): added pylint and mypy

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
-install_dev_dependencies:
+install_dev_dependencies: requirements/dev.txt
 	pip install -r requirements/dev.txt
 
-install_prod_dependencies:
+install_prod_dependencies: requirements/dev.txt
 	pip install -r requirements/prod.txt
 
-run_tests:
-	python -m pytest --verbose
+run_all_tests:
+	make unit_tests integration_tests linter type_checks
+
+unit_tests:
+	python -m pytest --verbose tests/unit --cov
+
+integration_tests:
+	python -m pytest --verbose tests/integration --cov
+
+linter:
+	pylint diffie_hellman_merkle
+
+type_checks:
+	mypy diffie_hellman_merkle
--- tests/unit/user/test_diffie_hellman_merkle.py
+++ tests/unit/user/test_diffie_hellman_merkle.py
@@ -4,16 +4,15 @@ from diffie_hellman_merkle.user import DiffieHellmanMerkle
 
 
 def test_generate_shared_secret():
-    """docstring TODO"""
     user1 = DiffieHellmanMerkle(
         shared_modulus=7_069_067_389,
         shared_base=69_420,
-        personal_secret=1234,
+        personal_secret=1_234,
     )
     user2 = DiffieHellmanMerkle(
         shared_modulus=7_069_067_389,
         shared_base=69_420,
-        personal_secret=4321,
+        personal_secret=4_321,
     )
     user1.generate_shared_secret(user2.value_to_share)
     user2.generate_shared_secret(user1.value_to_share)
