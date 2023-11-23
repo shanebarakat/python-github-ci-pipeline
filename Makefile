523e38c9666aa1b92a647cc15c7137cfba60e1ad
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2023-11-23 12:47:04 +0200

    test(test_end_to_end.py): added end-to-end test

--- Makefile
+++ Makefile
@@ -5,16 +5,19 @@ install_prod_dependencies: requirements/dev.txt
 	pip install -r requirements/prod.txt
 
 run_all_tests:
-	make unit_tests integration_tests end_to_end_tests linter type_checks
+	make test_coverage linter type_checks
+
+test_coverage:
+	python -m pytest --verbose --cov
 
 unit_tests:
-	python -m pytest --verbose tests/unit --cov
+	python -m pytest --verbose tests/unit
 
 integration_tests:
-	python -m pytest --verbose tests/integration --cov
+	python -m pytest --verbose tests/integration
 
 end_to_end_tests:
-	python -m pytest --verbose tests/test_end_to_end.py --cov
+	python -m pytest --verbose tests/test_end_to_end.py
 
 auto_format_code:
 	ruff format .
--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,7 +1,56 @@
-"""Testing encrypted communication using Diffie-Hellman-Merkle key exchange"""
+"""End-to-end tests of the whole python package"""
 
+# standard lib imports #
+import base64
+
+# 3rd party imports #
+import cryptography.fernet
+
+# project module imports #
 from diffie_hellman_merkle.user import DiffieHellmanMerkle
 
 
 def test_encrypted_communication():
-    assert 69 == 69
+    r"""Verifies that 2 users can communicate a secret encrypted message,
+    where the encryption key is communicated using Diffie-Hellman-Merkle
+    key exchange
+    """
+    public_shared_modulus: int = 7_069_067_389
+    public_shared_base: int = 247
+    original_message: str = "the secret channel is working nicely"
+    original_message_bytes: bytes = original_message.encode("utf-8")
+
+    user1 = DiffieHellmanMerkle(
+        shared_modulus=public_shared_modulus,
+        shared_base=public_shared_base,
+        personal_secret=80_085,
+    )
+    user2 = DiffieHellmanMerkle(
+        shared_modulus=public_shared_modulus,
+        shared_base=public_shared_base,
+        personal_secret=616_673,
+    )
+
+    # user1 encrypts message #
+    user1.generate_shared_secret(user2.value_to_share)
+    user1_fernet = cryptography.fernet.Fernet(
+        key=base64.urlsafe_b64encode(
+            str(user1.shared_secret).encode("utf-8").ljust(32)[:32]
+        )
+    )
+    encrypted_message: bytes = user1_fernet.encrypt(original_message_bytes)
+
+    # user2 decrypts message #
+    user2.generate_shared_secret(user1.value_to_share)
+    user2_fernet = cryptography.fernet.Fernet(
+        key=base64.urlsafe_b64encode(
+            str(user2.shared_secret).encode("utf-8").ljust(32)[:32]
+        )
+    )
+    decrypted_message_bytes: bytes = user2_fernet.decrypt(encrypted_message)
+    decrypted_message: str = decrypted_message_bytes.decode("utf-8")
+
+    # decrypted message must match original message #
+    assert (
+        decrypted_message == original_message
+    ), f'decrypted message "{decrypted_message}" does not match original message "{original_message}"'
